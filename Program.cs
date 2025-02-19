2502e44a184f58e4474613143e693f8150935a3a
Altaf <altafabdul721@gmail.com>
2025-02-19 16:33:10 +0530

    First commit with new console app

--- Program.cs
+++ Program.cs
@@ -1,12 +1,2 @@
-﻿using System;
-class public class Program
-{
-    public static void Main(string[] args)
-    {
-        // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!,hey when i was small u oouse to treat me so well ,and i like that quality in and ur deen");
-Console.WriteLine("Hello, World!,hey when i was small u oouse to treat me so well ,and i like that quality in and ur deen");
-Console.WriteLine("Hello, World!,hey when i was small u oouse to treat me so well ,and i like that quality in and ur deen");
-
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
